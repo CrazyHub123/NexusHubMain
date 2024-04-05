3f0930e5ed1681c3c9512b798906bcc812def042
CrazyHub <78809386+CrazyHub123@users.noreply.github.com>
2024-04-05 14:44:46 +0300

    Update Games.lua

--- Games.lua
+++ Games.lua
@@ -1,5 +1,7 @@
-Window:CreateLabel("Anime Punch Simulator")
-Window:CreateLabel("Anime Dungeon Fighters")
-Window:CreateLabel("One Fruit Simulator")
-Window:CreateLabel("Pet Catchers")
-Window:CreateLabel("Second Piece")
+return = {
+  Window:CreateLabel("Anime Punch Simulator")
+  Window:CreateLabel("Anime Dungeon Fighters")
+  Window:CreateLabel("One Fruit Simulator")
+  Window:CreateLabel("Pet Catchers")
+  Window:CreateLabel("Second Piece")
+}

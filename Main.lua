6ed51014ae7123fcda75c3806c3f71d205101db3
CrazyHub <78809386+CrazyHub123@users.noreply.github.com>
2024-04-20 23:11:22 +0300

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -2,17 +2,17 @@ local Place = game.PlaceId
 local Game = game.GameId
 
 if Place == 16914149804 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/CrazyHub123/NexusHubMain/main/AnimeSwordsX"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CrazyHub123/NexusHubMain/main/AnimeSwordsX.lua"))()
 elseif Place == 16733754732 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/CrazyHub123/NexusHubMain/main/AnimeIslands"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CrazyHub123/NexusHubMain/main/AnimeIslands.lua"))()
 elseif Game == 4069560710 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/CrazyHub123/NexusHubMain/main/OneFruitSimulator"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CrazyHub123/NexusHubMain/main/OneFruitSimulator.lua"))()
 elseif Place == 16510724413 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/CrazyHub123/NexusHubMain/main/PetCatchers"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CrazyHub123/NexusHubMain/main/PetCatchers.lua"))()
 elseif Place == 15776923116 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/CrazyHub123/NexusHubMain/main/AnimePunchSimulator"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CrazyHub123/NexusHubMain/main/AnimePunchSimulator.lua"))()
 elseif Place == 15049111150 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/CrazyHub123/NexusHubMain/main/SecondPiece"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CrazyHub123/NexusHubMain/main/SecondPiece.lua"))()
 elseif Place == 12135640403 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/CrazyHub123/NexusHubMain/main/AnimeDungeonFighters.lua"))()
 elseif Place == 15502802094 then

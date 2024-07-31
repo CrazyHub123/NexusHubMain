b32809f194e894ab1ec49d65c078d203a5bdcba4
CrazyHub <78809386+CrazyHub123@users.noreply.github.com>
2024-08-01 02:37:00 +0300

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -2,30 +2,14 @@
 local Place = game.PlaceId
 local Game = game.GameId
 
-if Place == 16914149804 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/CrazyHub123/NexusHubMain/main/AnimeSwordsX.lua"))()
-elseif Place == 16733754732 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/CrazyHub123/NexusHubMain/main/AnimeIslands.lua"))()
-elseif Game == 4069560710 then
+if Game == 4069560710 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/CrazyHub123/NexusHubMain/main/OneFruitSimulator.lua"))()
-elseif Place == 16510724413 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/CrazyHub123/NexusHubMain/main/PetCatchers.lua"))()
-elseif Place == 15776923116 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/CrazyHub123/NexusHubMain/main/AnimePunchSimulator.lua"))()
-elseif Place == 6897167394 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/CrazyHub123/NexusHubMain/main/DemonPiece.lua"))()
-elseif Place == 15049111150 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/CrazyHub123/NexusHubMain/main/SecondPiece.lua"))()
-elseif Place == 12135640403 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/CrazyHub123/NexusHubMain/main/AnimeDungeonFighters.lua"))()
-elseif Place == 15502802094 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/CrazyHub123/NexusHubMain/main/AnimeDungeonFightersSecond.lua"))()
-elseif Place == 15742087191 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/CrazyHub123/NexusHubMain/main/AnimeDungeonFightersThird.lua"))()
-elseif Place == 14752086859 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/CrazyHub123/NexusHubMain/main/AnimeMaxSimulator.lua"))()
 elseif Place == 15705682243 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/CrazyHub123/NexusHubMain/main/TappingLegendsFinal.lua"))()
 elseif Place == 16741537517 then
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/CrazyHub123/NexusHubMain/main/AnimeHeroes.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CrazyHub123/NexusHubMain/main/AnimeHeroes.lua"))()
+elseif Game == 6216468795 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CrazyHub123/NexusHubMain/main/WeakLegacy.lua"))()
+elseif Game == 5170731021 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CrazyHub123/NexusHubMain/main/DemonBlade.lua"))()
 end
